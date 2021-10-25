7ec62eee6ba0a5dc8ec337863ea228677604e066
김민주 <mj1584.mk@gmail.com>
2021-10-26 00:19:00 +0900

    스택을 이용한 중위식을 후위식으로 변경

---
+++ Infix_To_Postfix_By_Stack.py
@@ -0,0 +1,46 @@
+import StackByListUsingClass as sc
+import PostfixByStack as ps
+
+infix1 = ['8', '/', '2', '-', '3', '+', '(', '3', '*', '2', ')']
+
+def Precedence(op):
+    if op == '(' or op ==')':
+        return 0
+    elif op == '+' or op == '-':
+        return 1
+    elif op == '*' or op == '/':
+        return 2
+    else:
+        return -1
+
+def InToPost(exrp):
+    ITP = sc.Stack()
+    output = []
+    for ch in exrp:
+        if ch == '(':
+            ITP.Push(ch)
+        elif ch == ')':
+            while not ITP.IsEmpty():
+                op = ITP.Pop()
+                if op == '(':
+                    break
+                else:
+                    output.append(op)
+        elif ch in "+-*/":
+            while not ITP.IsEmpty():
+                op = ITP.Peek()
+                if (Precedence(op) >= Precedence(ch)):
+                    output.append(op)
+                    ITP.Pop()
+                else:
+                    break
+            ITP.Push(ch)
+        else:
+            output.append(ch)
+    while not ITP.IsEmpty():
+        output.append(ITP.Pop())
+    return output
+
+print("중위 표기: ", infix1)
+print("후위 표기: ", InToPost(infix1))
+print("연산 결과: ", ps.Postfix(InToPost(infix1)))
