db5d4ad54557ceb35a6974f1f706c54b67407a08
김민주 <mj1584.mk@gmail.com>
2021-11-14 22:20:37 +0900

    딕셔너리를 이용한 단어장

---
+++ My_WordBook_By_Dictionary.py
@@ -0,0 +1,20 @@
+d = {}
+d['data'] = '자료'
+d['structure'] = '구조'
+d['sequential search'] = '순차 탐색'
+d['game'] = '게임'
+d['binary search'] = '이진 탐색'
+
+print("나의 단어장 : ")
+print(d)
+
+if d.get('game') :
+    print("탐색 : game ->", d['game'])
+if d.get('over'):
+    print("탐색 : game ->", d['over'])
+if d.get('data'):
+    print("탐색 : game ->", d['data'])
+
+d.pop('game')
+print("나의 단어장 : ")
+print(d)
