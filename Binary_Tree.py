827dbba6ab0d221a09573e5166a9663f1a56d714
김민주 <mj1584.mk@gmail.com>
2021-11-18 21:58:44 +0900

    이진 트리, 노드 레벨, 트리 전체 경로 수, 균형 트리 판단 여부 함수 추가

--- Binary_Tree.py
+++ Binary_Tree.py
@@ -1,4 +1,5 @@
 import CircleQueue_By_Python as CQ
+import math
 
 class TNode:
     def __init__(self, data, left, right):
@@ -9,6 +10,12 @@ class TNode:
 def Pre_order(n):
     if n is not None:
         print(n.data, end = '')
+        Pre_order(n.left)
+        Pre_order(n.right)
+
+def Pre_order2(n):
+    if n is not None:
+
         Pre_order(n.left)
         Pre_order(n.right)
 
@@ -58,25 +65,70 @@ def Calc_height(n):
     else:
         return hright + 1
 
+def Is_balanced(root):
+    if root == None:
+        return 1, 'True'
+    hleft, balance = Is_balanced(root.left)
+    hright, balance = Is_balanced(root.right)
+    if hleft-hright > -2 and hleft - hright < 2 and balance == 'True':
+        if hleft > hright:
+            return hleft + 1, 'True'
+        else:
+            return hright + 1, 'True'
+    else:
+        if hleft > hright:
+            return hleft + 1, 'False'
+        else:
+            return hright + 1, 'False'
+
+def level(root, node):
+    sum = 0
+    queue = CQ.Circle_Queue()
+    queue.enQueue(root)
+    while not queue.isEmpty():
+        n = queue.deQueue()
+        sum += 1
+        if n is not None:
+            queue.enQueue(n.left)
+            queue.enQueue(n.right)
+            if n.data == node:
+                return int(math.log2(sum)) + 1
+
+
+
+def path_length(root):
+    length = 0
+    queue = CQ.Circle_Queue()
+    queue.enQueue(root)
+    while not queue.isEmpty():
+        n = queue.deQueue()
+        if n is not None:
+            node_level = level(root, n.data) - 1
+            length += node_level
+            queue.enQueue(n.left)
+            queue.enQueue(n.right)
+
+    return length
+
 
 if __name__ == '__main__':
+    c = TNode('C', None, None)
     d = TNode('D', None, None)
-    e = TNode('E', None, None)
-    b = TNode('B', d, e)
     f = TNode('F', None, None)
-    c = TNode('C', f, None)
-    root = TNode('A', b, c)
+    b = TNode('B', c, d)
+    e = TNode('E', None, f)
+    root = TNode('A', b, e)
 
-    print('\n In-Order : ', end = '')
+    print('In-Order : ', end = '')
     In_order(root)
 
-    print('\n Pre-Order : ', end = '')
+    print('\nPre-Order : ', end = '')
     Pre_order(root)
 
-    print('\n Post-Order : ', end = '')
+    print('\nPost-Order : ', end = '')
     Post_order(root)
 
-    print('\n Level-Order : ', end = '')
+    print('\nLevel-Order : ', end = '')
     Level_order(root)
 
     print()
@@ -84,3 +136,6 @@ if __name__ == '__main__':
     print("노드의 개수 = %d개" % Count_node(root))
     print("단말의 개수 = %d개" % Count_leaf(root))
     print("트리의 높이 = %d" % Calc_height(root))
+    print("C 노드의 레벨 : %d" % level(root, 'C'))
+    print("균형 트리 :", Is_balanced(root)[1])
+    print("전체 경로의 길이 : %d" % path_length(root))
